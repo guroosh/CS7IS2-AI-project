667847603804cf493e064a6670585e7b3e4b16ed
guroosh <csingh@tcd.ie>
2020-03-16 12:45:00 +0000

    fixed df error

--- RunDFS.py
+++ RunDFS.py
@@ -11,16 +11,16 @@ def dfs(grid_world, key):
     x = int(key.split(',')[0])
     y = int(key.split(',')[1])
     if x == grid_world.end_x and y == grid_world.end_y:
-        grid_world.route.append((x, y))
-        grid_world.final_route.append((x, y))
+        grid_world.dfs_route.append((x, y))
+        grid_world.dfs_best_route.append((x, y))
         return -1
     grid_world.is_visited[x][y] = 1
-    grid_world.route.append((x, y))
+    grid_world.dfs_route.append((x, y))
     for l in adjacent_nodes:
         if grid_world.is_visited[l[0]][l[1]] == 0:
             ret_val = dfs(grid_world, str(l[0]) + "," + str(l[1]))
             if ret_val == -1:
-                grid_world.final_route.append((l[0], l[1]))
+                grid_world.dfs_best_route.append((l[0], l[1]))
                 return -1
 
 
@@ -30,25 +30,25 @@ def random_dfs(grid_world, key):
     x = int(key.split(',')[0])
     y = int(key.split(',')[1])
     if x == grid_world.end_x and y == grid_world.end_y:
-        grid_world.route.append((x, y))
-        grid_world.final_route.append((x, y))
+        grid_world.dfs_route.append((x, y))
+        grid_world.dfs_best_route.append((x, y))
         return -1
     grid_world.is_visited[x][y] = 1
-    grid_world.route.append((x, y))
+    grid_world.dfs_route.append((x, y))
     random.shuffle(adjacent_nodes)
     for l in adjacent_nodes:
         if grid_world.is_visited[l[0]][l[1]] == 0:
             ret_val = random_dfs(grid_world, str(l[0]) + "," + str(l[1]))
             if ret_val == -1:
-                grid_world.final_route.append((l[0], l[1]))
+                grid_world.dfs_best_route.append((l[0], l[1]))
                 return -1
 
 
 def run_dfs(grid_world):
     # dfs(grid_world, grid_world.start_key)
     random_dfs(grid_world, grid_world.start_key)
-    grid_world.final_route.append((grid_world.start_x, grid_world.start_y))
-    grid_world.final_route = grid_world.final_route[::-1]
+    grid_world.dfs_best_route.append((grid_world.start_x, grid_world.start_y))
+    grid_world.dfs_best_route = grid_world.dfs_best_route[::-1]
 
 
 grid_world = GridWorld()
