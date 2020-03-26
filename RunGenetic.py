800b8f4dbcd9ff516f682f12a13b00d5d06b6f80
kavi22493@gmail.com <kavi22493@gmail.com>
2020-03-26 13:56:34 +0000

    Added cross over code Genetic algorithm

--- RunGenetic.py
+++ RunGenetic.py
@@ -3,7 +3,6 @@ import FunctionsGenetic
 from GridWorldGenetic import GridWorld
 import tkinter as tk
 
-
 def get_random_path(self, start_node, end_node, graph):
     def recursive_function(key):
         adjacent_nodes = graph.adjacency_map[key]
@@ -38,12 +37,11 @@ class Genetic(object):
     def run(self, graph):
         path=[]
         i=1
-        iteration=21
+        iteration=20
         for i in range(iteration):
             paths=get_random_path(self, [0,0], [4,4], graph)
             path.append(paths)
         return path
-
 
 grid_world = GridWorld()
 FunctionsGenetic.create_random_obstacles(grid_world, 0.05)
@@ -56,3 +54,53 @@ grid_world.create_grid_ui(grid_world.m, grid_world.n, (grid_world.start_x, grid_
 
 #Population paths list
 path = Genetic().run(graph)
+#print(path)
+first_list=[]
+second_list=[]
+while path:
+    count=len(path)
+    first_pop=path[count-1]
+    first_list.append(first_pop)
+    path.pop(-1)
+    sec_pop=path[0]
+    second_list.append(sec_pop)
+    path.pop(0)
+    count=count+1
+
+for i in first_list:
+    i_count=len(i)
+    j_count=0
+    rand_idx = random.sample(i, 1)
+    for j in second_list:
+        while j_count<len(j):
+            for k in j:
+                if tuple(rand_idx)==k:
+                    print(rand_idx)
+                    break;
+                j_count=j_count+1
+        if j_count>len(j):
+            rand_idx = random.sample(i, 1)
+            j_count =0
+
+
+    # for j in second_list:
+    #     j_count=0
+    #     rand_idx = random.sample(i[0], 6)
+        #rand_idx = random.choice(i[0])
+        #rand_idx = random.randrange(i_count)
+        # print(rand_idx)
+        # if rand_idx in chain(*j[j_count]):
+        #     print("rand", rand_idx)
+        # while i[i_count-1]:
+        #     if i[i_count-1] == j[j_count-1]:
+        #         print("i equal")
+        #         break;
+        #     # i_count=i_count-1
+        #     # j_count=j_count-1
+
+# lst = []
+# for index,letter in enumerate(path):
+#     lst.append(letter) # add 'a' to your empty list
+#     path.pop(index) # remove 'a' from the original string
+#     print(lst)
+
